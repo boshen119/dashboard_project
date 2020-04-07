23a0421c255d315451aab895a74f0cf3675e85fe
boshen119 <56060467+boshen119@users.noreply.github.com>
2020-04-06 22:00:20 -0700

    updating bar chart

--- logic.js
+++ logic.js
@@ -4,10 +4,59 @@
 // Create map object:
 
 // Plotly.
-d3.json('/static/loan_sum_avg_cs.json', function(data){
-  console.log("Hello");
-    console.log(data);
+// declaring SVG params
+var svg = d3.select('svg');
+var width = +svg.attr('width');
+var height = +svg.attr('height');
+
+//starting the function
+var render = data => {
+  var margin = {top: 20, right: 40, bottom: 20, left: 100};
+  var innerWidth = width - margin.left - margin.right;
+  var innerHeight = height - margin.top - margin.bottom;
+
+  var xscale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.sum_loaned)])
+    .range([0, innerWidth]);
+
+  var yscale = d3.scaleBand()
+    .domain(data.map(d => d.addr_state))
+    .range([0, innerHeight]);
+
+    // Appending the bar traits to 'g'
+    var g = svg.append("g")
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+    g.append('g').call(d3.axisLeft(yscale));
+    g.append('g').call(d3.axisBottom(xscale))
+    .attr('transform', `translate(0, ${innerHeight})`);
+
+    g.selectAll('rect').data(data)
+    .enter().append('rect')
+    .attr('y', d => yscale(d.addr_state))
+    .attr('width', d => xscale(d.sum_loaned))
+    .attr('height', yscale.bandwidth());
+
+};
+
+
+  d3.csv('/static/new_sum_and_avg.csv').then(data=> {
+    // console.log(data);
+    data.forEach(d => {
+      d.sum_loaned = +d.sum_loaned;
+      d.avg_loan = +d.avg_loan;
+      d.avg_annual_inc = +d.avg_annual_inc;
+      d.avg_fico_range_low = +d.avg_fico_range_low;
+      d.avg_fico_range_high = +d.avg_fico_range_high;
+      console.log(d);
+    })
+    render(data);
   });
+
+
+// d3.json('/static/loan_sum_avg_cs.json', function(data){
+//   console.log("Hello");
+//     console.log(data);
+//   });
     //   function unpack(rows, key) {
     //       return rows.map(function(row) { return row[key]; });
 
